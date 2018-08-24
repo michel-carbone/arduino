34fc1d7b57473d3a78a07336c3519687a5b46f02
michel <michel@arsalis.com>
2018-08-24 08:58:38 +0200

    add names for pin-out of LCD Keypad pins and add comments for clarification

--- sketchbook/lcd_keypad_DFR0009/lcd_keypad_DFR0009.ino
+++ sketchbook/lcd_keypad_DFR0009/lcd_keypad_DFR0009.ino
@@ -5,27 +5,42 @@
 
 This program will test the LCD panel and the buttons
 Mark Bramwell, July 2010
-
+Edits by Michel Carbone, Augustus 2018
 ********************************************************/
+/* convention of pin naming:
+*  device, e.g. LCD
+*  pin type & direction at module, e.g. DO for Digital Output
+*  pin name at module location
+ */
+// pin-out of LCD Keypad VMA203
+#define LCD_AO_AnalogButtons  0
+#define LCD_DO_DB4 4
+#define LCD_DO_DB5 5
+#define LCD_DO_DB6 6
+#define LCD_DO_DB7 7
+#define LCD_DO_RS 8
+#define LCD_DO_EN 9
+#define LCD_DO_BacklitCtrl 10
 
 // select the pins used on the LCD panel
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+LiquidCrystal lcd(LCD_DO_RS, LCD_DO_EN, LCD_DO_DB4, LCD_DO_DB5, LCD_DO_DB6, LCD_DO_DB7);
 
 // define some values used by the panel and buttons
-int lcd_key     = 0;
-int adc_key_in  = 0;
+int lcd_key     = 0; //index of pushed button
+int adc_key_in  = 0; //analog value read by the ADC
+// user friendly name for value to store in lcd_key variable
 #define btnRIGHT  0
 #define btnUP     1
 #define btnDOWN   2
 #define btnLEFT   3
 #define btnSELECT 4
-#define btnNONE   5
+#define btnNONE   5 // default value
 
 // read the buttons
 int read_LCD_buttons()
 {
- adc_key_in = analogRead(0);      // read the value from the sensor 
- // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+ adc_key_in = analogRead(LCD_AO_AnalogButtons);      // read the value from the sensor 
+ // my buttons when read are centered at these values: 0, 144, 329, 504, 741
  // we add approx 50 to those values and check to see if we are close
  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
  // For V1.1 us this threshold
