178377d89c9a96c7f6ce7eca978a588e3e11cfff
michel <michel@arsalis.com>
2018-09-24 18:43:36 +0200

    add measure of loop cycle time in microsecond

--- sketchbook/RS232_console_test/RS232_console_test.ino
+++ sketchbook/RS232_console_test/RS232_console_test.ino
@@ -12,15 +12,24 @@ void setup() {
 
 // variable for loop() pointer
 long i = 0; // pointer
+// variables in loop() for precise timing
+long tStart = 0;    // time in us before first Serial Print
+long tStop = 0;     // time in us after last Serial Print 
+long tTime = 0;     // time between tStart and tStop
 
 // function loop()
 // is call automaticaly after setup() and run forever after setup
 void loop() {
   // put your main code here, to run repeatedly:
+  tStart = micros();
   Serial.print("Step number ");
-  Serial.print(i);
-  Serial.print("; time elapsed: ");
-  Serial.println(millis());
+  Serial.print(i); // display loop count
+  Serial.print("; time elapsed (ms): ");
+  Serial.print(millis()); // display current time from startup
+  Serial.print("; duration (us): "); // 62 char takes around 1052 us to print to COM port
+  Serial.println(tTime); // display duration of previous loop cycle
   i=i+1;
-  delay(10);
+  tStop = micros();
+  tTime = tStop - tStart; // calc diff  of current loop cycle
+  delay(99); // 99 in order to remove the approx. 1ms for serial print
 }
